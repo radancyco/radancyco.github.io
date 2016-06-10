3f3eb2bb22e72719fb68a6f5e49218cc7cf5b07c
Spellacy, Michael <Michael.Spellacy@tmp.com>
2016-06-10 14:51:52 -0400

    Typos

--- uid/bookmarklets/scripts/1.0/themes-pages/highlight-page-type.js
+++ uid/bookmarklets/scripts/1.0/themes-pages/highlight-page-type.js
@@ -1,7 +1,7 @@
 
 if(!$("#css-bookmarklet").length) {
 
-	$('head').append('<link rel="stylesheet" id="css-bookmarklet" href="https://dl.dropboxusercontent.com/u/58819/talentbrew-bookmarklets/scripts/1.0/bookmarklets.css"/>');
+	$('head').append('<link rel="stylesheet" id="css-bookmarklet" href="https://tmpworldwide.github.io/uid/bookmarklets/scripts/1.0/bookmarklets.css"/>');
 
 }
 

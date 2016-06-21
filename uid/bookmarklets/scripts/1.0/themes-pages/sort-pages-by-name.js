af84671e12492fd2efde29ed0864b06b7e01c572
Spellacy, Michael <Michael.Spellacy@tmp.com>
2016-06-21 19:10:22 -0400

    Separated Sort & Append URL Bookmarklets

--- uid/bookmarklets/scripts/1.0/themes-pages/sort-pages-by-name.js
+++ uid/bookmarklets/scripts/1.0/themes-pages/sort-pages-by-name.js
@@ -1,5 +1,7 @@
 // TMP Worldwide Bookmarklet: Sort Pages by Name
 
+alert('URL appending is its own function now! Please visit https://tmpworldwide.github.io/uid/bookmarklets/ and bookmark it!');
+
 $('#page-list tbody').each(function(index) {
 
 	$(this).find('tr').sort(function (a, b) {
@@ -15,49 +17,3 @@ $('#page-list tbody').each(function(index) {
 	$(this).find('td:first-child a').attr('target', '_blank');
 
 });
-
-// Dropping Jorge's code in...
-
-var pages = $('[data-page-type="content"]');
-var pageCount = pages.length;
-
-for (var i = 0; i <= pageCount; i++) {
-
-	(function(i) {
-
-		var url = $(pages[i]).find('a').attr('href');
-		var vanity = "";
-
-		$.ajax({
-
-			url: url,
-			type: 'GET',
-			success: function(data) {
-
-				vanity = $(data).find('#vanity-url').val();
-				redirect = $(data).find('#RedirectUrl').val();
-				domain = $(data).find('#vanity-url').prev().text();
-
-				displayVanity(domain, vanity, redirect, i);
-
-			}
-
-		});
-
-	})(i);
-
-}
-
-function displayVanity(clientDomain, vanityURL, redirectURL, i) {
-
-	if (vanityURL == "") {
-
-		$(pages[i]).children(":eq(1)").append('<p class=theme-url><a href=' + redirectURL + ' target=_blank>' + redirectURL + '</a>');
-
-	} else {
-
-		$(pages[i]).children(":eq(1)").append('<p class=theme-url><a href=https://' + clientDomain + vanityURL + ' target=_blank>' + 'https://' + clientDomain + vanityURL + '</a></p>');
-
-	}
-
-}

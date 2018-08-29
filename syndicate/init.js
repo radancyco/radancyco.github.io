f8ac5ed0f0239d941446dcb165d8dd3bdccc0e00
Spellacy, Michael <spell@spellacy.net>
2018-08-29 16:35:26 -0400

    Added example usage

--- syndicate/init.js
+++ syndicate/init.js
@@ -6,6 +6,28 @@ fetch(pagePath).then(function(response) {
 
 }).then(function(body) {
 
-  document.querySelector('#tmp-syndicate').innerHTML = body;
+  document.querySelector("#tmp-syndicate").innerHTML = body;
 
 });
+
+// Example Usage
+
+/* @model PublishedContentModel
+
+@{
+    ViewBag.Title = Model.PageTitle;
+    ViewBag.MetaDescription = Model.PageDescription;
+    ViewBag.BodyId = "support";
+    ViewBag.PrimaryHeading = ViewBag.Title;
+    ViewBag.TertiaryLayout = "true";
+}
+
+<div id="tmp-syndicate">
+
+    <a id="tmp-syndicate-link" href="https://tmpworldwide.github.io/support/"><noscript>View TMP Support</noscript></a>
+
+</div>
+
+<script src="https://tmpworldwide.github.io/syndicate/init.js"></script>
+
+*/

582cf10967f1767ffdb6d4758bc378e2db3606c6
Spellacy, Michael <spell@spellacy.net>
2019-07-15 13:15:03 -0400

    Yesy

---
+++ news/init.js
@@ -0,0 +1,12 @@
+/*
+
+TMP News
+  Developer: Michael "Spell" Spellacy, Email: michael.spellacy@tmp.com, Twitter: @spellacy, GitHub: michaelspellacy
+
+*/
+
+(function() {
+
+  console.log("Test");
+
+})();

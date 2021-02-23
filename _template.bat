8de92d0cb16d5e1cb05726bdcd1544d5c40cbd73
michaelspellacy <spell@spellacy.net>
2021-02-22 20:33:54 -0500

    Template Copier, Jekyll

---
+++ _template.bat
@@ -0,0 +1,5 @@
+xcopy /y _layouts\default.html ..\pattern-library\_layouts\
+xcopy /y _layouts\default.html ..\career-path-generator\_layouts\
+xcopy /y _layouts\default.html ..\tb-ajd-template\_layouts\
+xcopy /y _layouts\default.html ..\tmp-custom-imports\_layouts\
+xcopy /y _layouts\default.html ..\tmp-magic-bullet\_layouts\

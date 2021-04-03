d2adc51f5df420aff4c71a3aa9743c17312af76e
michaelspellacy <spell@spellacy.net>
2021-04-03 10:39:08 -0400

    Accessibility Extension
    Initial Revision

---
+++ a11y/extension/init.js
@@ -0,0 +1,56 @@
+// Accessibility Checklist
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  chrome.tabs.query({
+
+    active: true, lastFocusedWindow: true}, tabs => {
+
+    let url = tabs[0].url;
+
+    var A11yLink = document.getElementById("wave-validation");
+    var A11yHref = "https://wave.webaim.org/report#/";
+    A11yLink.setAttribute("href", A11yHref + url)
+
+    var HTMLLink = document.getElementById("html-validation");
+    var HTMLHref = "https://validator.w3.org/nu/?doc=";
+    HTMLLink.setAttribute("href", HTMLHref + url);
+
+    var CSSLink = document.getElementById("css-validation");
+    var CSSHref = "https://jigsaw.w3.org/css-validator/validator?profile=css3&warning=0&uri=";
+    CSSLink.setAttribute("href", CSSHref + url);
+
+  });
+
+  function exampleFunction(scriptName) {
+
+    chrome.tabs.executeScript({
+
+      code: "var scriptName = " + JSON.stringify(scriptName) },
+
+      function() {
+
+        chrome.tabs.executeScript({ file: "page.js" }); // this is key
+        // chrome.tabs.insertCSS({ file: "page.css" });
+
+      }
+
+    )
+
+  }
+
+  var a11yButtons = document.querySelectorAll("button[data-script]");
+
+  a11yButtons.forEach(function(button, j){
+
+    button.addEventListener("click", (e) => {
+
+      let dataScript = button.getAttribute("data-script");
+
+      exampleFunction(dataScript);
+
+    });
+
+  });
+
+});
---
+++ a11y/extension/page.js
@@ -0,0 +1,16 @@
+
+// document.body.classList.add(className);
+
+document.body.appendChild(document.createElement("script")).src=scriptName;
+
+/* if(scriptName == "iframes") {
+
+  var iframes=document.getElementsByTagName("iframe");
+
+  for(i=0;i<iframes.length;i++) {
+
+    iframes[i].contentDocument.body.appendChild(document.createElement("script")).src=scriptName;
+
+  }
+
+} */

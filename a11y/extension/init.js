79e2e3ba6c002bddd9087d88d23cfc9ee3281496
michaelspellacy <spell@spellacy.net>
2021-04-05 18:59:34 -0400

    Test

--- a11y/extension/init.js
+++ a11y/extension/init.js
@@ -10,20 +10,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let url = tabs[0].url;
 
-    var A11yLink = document.getElementById("wave-validation");
-    var A11yHref = "https://wave.webaim.org/report#/";
+    let A11yLink = document.getElementById("validate-a11y");
+    let A11yHref = "https://wave.webaim.org/report#/";
     A11yLink.setAttribute("href", A11yHref + url)
 
-    var HTMLLink = document.getElementById("html-validation");
-    var HTMLHref = "https://validator.w3.org/nu/?showsource=yes&showoutline=yes&showimagereport=yes&doc=";
+    let HTMLLink = document.getElementById("validate-html");
+    let HTMLHref = "https://validator.w3.org/nu/?showsource=yes&showoutline=yes&showimagereport=yes&doc=";
     HTMLLink.setAttribute("href", HTMLHref + url);
 
-    var CSSLink = document.getElementById("css-validation");
-    var CSSHref = "https://jigsaw.w3.org/css-validator/validator?profile=css3&warning=0&uri=";
+    let CSSLink = document.getElementById("validate-css");
+    let CSSHref = "https://jigsaw.w3.org/css-validator/validator?profile=css3&warning=0&uri=";
     CSSLink.setAttribute("href", CSSHref + url);
 
-    var PDFLink = document.getElementById("pdf-validation");
-    var PDFHref = "http://checkers.eiii.eu/en/pdfcheck/?url=";
+    let PDFLink = document.getElementById("validate-pdf");
+    let PDFHref = "http://checkers.eiii.eu/en/pdfcheck/?url=";
     PDFLink.setAttribute("href", PDFHref + url);
 
   });
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   )};
 
-  var a11yButtons = document.querySelectorAll("button[data-script]");
+  let a11yButtons = document.querySelectorAll("button[data-script]");
 
   a11yButtons.forEach(function(button, j){
 
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   }
 
-  var resetButton = document.getElementById("btn-reset");
+  let resetButton = document.getElementById("btn-reset");
 
   resetButton.addEventListener("click", () => {
 

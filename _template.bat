b0d9ed03d04db6cb2a69c659c5b6fc7b353a5739
michaelspellacy <spell@spellacy.net>
2021-07-29 14:00:10 -0400

    A11y Reconfig

--- _template.bat
+++ _template.bat
@@ -1,3 +1,4 @@
+xcopy /y _layouts\default.html ..\a11y\_layouts\
 xcopy /y _layouts\default.html ..\pattern-library\_layouts\
 xcopy /y _layouts\default.html ..\career-path-generator\_layouts\
 xcopy /y _layouts\default.html ..\tb-ajd-template\_layouts\

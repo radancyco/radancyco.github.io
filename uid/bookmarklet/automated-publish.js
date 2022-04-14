dc019d5138568d7518c2f1452104c82ff7550076
Michael Spellacy (Spell) <spell@spellacy.net>
2022-04-14 17:40:48 -0400

    Update automated-publish.js

--- uid/bookmarklet/automated-publish.js
+++ uid/bookmarklet/automated-publish.js
@@ -1,19 +1,25 @@
 function submitPage() {
 
+  // Remove all checkboxes we don't want checked in publishing.
+
   $('.select-all, #staged-sass, #staged-javascript').remove();
 
   if($('.publish-table :checkbox').length) {
 
     var n = $('.publish-table :checkbox').length;
 
-    console.log('%c Publishing. There are ' + n + ' pages to go.', 'background: #6e00ee; color: #fff');
+    console.log('%c Publishing. There are ' + n + ' assets to go...', 'background: #6e00ee; color: #fff');
 
     $('.publish-table :checkbox:lt(10)').prop('checked', true);
     $('.admin-ajax-submit').removeAttr('disabled').submit();
 
   } else {
 
-    console.log('%c Finished publishing! Be sure to publish Sass and JavaScript manually.', 'background: #009e60; color: #fff');
+    // Sass and JavaScript are sometimes not removed from the publishing queue, so we need to bypass them and remind user to publish manually.
+
+    console.log('%c Finished publishing! Be sure to publish Sass and JavaScript manually. Have a nice day!', 'background: #009e60; color: #fff');
+
+    // exit function
 
     return;
 

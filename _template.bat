77e2082f8f434350c4cf21534fe3197e30d71591
Michael Spellacy (Spell) <spell@spellacy.net>
2026-01-14 12:13:18 -0500

    Maint.

--- _template.bat
+++ _template.bat
@@ -1,6 +1,6 @@
 REM Running batch to copy files over to other project directories...
 
-REM Template
+REM Layout
 
 xcopy /y _layouts\default.html ..\a11y\_layouts\
 xcopy /y _layouts\default.html ..\component-library\_layouts\
@@ -10,3 +10,25 @@ xcopy /y _layouts\default.html ..\career-path-generator\_layouts\
 xcopy /y _layouts\default.html ..\tb-ajd-template\_layouts\
 xcopy /y _layouts\default.html ..\tmp-custom-imports\_layouts\
 xcopy /y _layouts\default.html ..\magicbullet\_layouts\
+
+REM Template
+
+xcopy /y _includes\template.html ..\a11y\_includes\
+xcopy /y _includes\template.html ..\component-library\_includes\
+xcopy /y _includes\template.html ..\playground\_includes\
+xcopy /y _includes\template.html ..\radancyco.github.io\_includes\
+xcopy /y _includes\template.html ..\career-path-generator\_includes\
+xcopy /y _includes\template.html ..\tb-ajd-template\_includes\
+xcopy /y _includes\template.html ..\tmp-custom-imports\_includes\
+xcopy /y _includes\template.html ..\magicbullet\_includes\
+
+REM Icon
+
+xcopy /y _includes\icon.html ..\a11y\_includes\
+xcopy /y _includes\icon.html ..\component-library\_includes\
+xcopy /y _includes\icon.html ..\playground\_includes\
+xcopy /y _includes\icon.html ..\radancyco.github.io\_includes\
+xcopy /y _includes\icon.html ..\career-path-generator\_includes\
+xcopy /y _includes\icon.html ..\tb-ajd-template\_includes\
+xcopy /y _includes\icon.html ..\tmp-custom-imports\_includes\
+xcopy /y _includes\icon.html ..\magicbullet\_includes\

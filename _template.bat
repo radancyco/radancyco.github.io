1ed64c10825f12177dd9d896347b0b0437f316e5
Michael Spellacy (Spell) <spell@spellacy.net>
2026-06-18 11:29:27 -0400

    Template Update

--- _template.bat
+++ _template.bat
@@ -2,6 +2,7 @@ REM Running batch to copy files over to other project directories...
 
 REM Layout
 
+xcopy /y _layouts\default.html ..\a11y\_layouts\
 xcopy /y _layouts\default.html ..\component-library\_layouts\
 xcopy /y _layouts\default.html ..\playground\_layouts\
 xcopy /y _layouts\default.html ..\radancyco.github.io\_layouts\
@@ -12,6 +13,7 @@ xcopy /y _layouts\default.html ..\magicbullet\_layouts\
 
 REM Template
 
+xcopy /y _includes\template.html ..\a11y\_includes\
 xcopy /y _includes\template.html ..\component-library\_includes\
 xcopy /y _includes\template.html ..\playground\_includes\
 xcopy /y _includes\template.html ..\radancyco.github.io\_includes\
@@ -22,6 +24,7 @@ xcopy /y _includes\template.html ..\magicbullet\_includes\
 
 REM Icon
 
+xcopy /y _includes\icon.html ..\a11y\_includes\
 xcopy /y _includes\icon.html ..\component-library\_includes\
 xcopy /y _includes\icon.html ..\playground\_includes\
 xcopy /y _includes\icon.html ..\radancyco.github.io\_includes\
@@ -32,6 +35,7 @@ xcopy /y _includes\icon.html ..\magicbullet\_includes\
 
 REM Permalinks
 
+xcopy /y _includes\permalink.html ..\a11y\_includes\
 xcopy /y _includes\permalink.html ..\component-library\_includes\
 xcopy /y _includes\permalink.html ..\playground\_includes\
 xcopy /y _includes\permalink.html ..\radancyco.github.io\_includes\
@@ -42,6 +46,7 @@ xcopy /y _includes\permalink.html ..\magicbullet\_includes\
 
 REM Components (For Navigation)
 
+xcopy /y _data\module.yml ..\a11y\_data\
 xcopy /y _data\module.yml ..\component-library\_data\
 xcopy /y _data\module.yml ..\playground\_data\
 xcopy /y _data\module.yml ..\radancyco.github.io\_data\
